3d4df14a959ded855abd489ef2fcdd2c118dda81
Nafees <abdur17@student.sust.edu>
2022-05-24 08:11:56 +0600

    delete done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -19,12 +19,14 @@ async function run() {
     try {
         await client.connect();
         const toolsCollection = client.db('autocar-tools').collection('products');
+        const orderCollection = client.db('autocar-tools').collection('orders');
+        const userCollection = client.db('autocar-tools').collection('users');
 
         app.get('/tools', async (req, res) => {
             const query = {};
             const cursor = toolsCollection.find(query);
-            const services = await cursor.toArray();
-            res.send(services);
+            const tools = await cursor.toArray();
+            res.send(tools);
 
         })
 
@@ -34,6 +36,46 @@ async function run() {
             const result = await toolsCollection.findOne(query);
             res.send(result);
         });
+
+        app.put('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = {email: email};
+            const options = {upsert: true};
+            const updatedDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        });
+
+        // POST neew items
+
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            console.log('adding new item', order);
+            const result = await orderCollection.insertOne(order);
+            res.send(result)
+        });
+
+
+        app.get('/orders', async (req, res) => {
+            const email = req.query.email;
+            const query = {email: email};
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+
+        })
+
+        // delete an item
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
         console.log('connected db');
 
@@ -53,3 +95,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
 })
+
+// const http = require('http'); 
+// const server = http.createServer(app); 
+// server.listen(process.env.PORT);
